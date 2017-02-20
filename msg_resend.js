b3773d9472eb1dce65a72c0e936f31c95ee99915
bartbutenaers <bart.butenaers@gmail.com>
2017-02-20 21:31:31 +0100

    Control via msg

--- msg_resend.js
+++ msg_resend.js
@@ -42,17 +42,60 @@ module.exports = function(RED) {
         this.on('input', function(msg) {
             node.msgCounter = 0;
 
-            // As soon as a message arrives, it will be cloned to the output
-            sendMsg(msg, node);
-
-            var msgTimestamp = Date.now();
-            
-            if((msgTimestamp - node.prevMsgTimestamp) <= node.interval) {
-                node.status({fill:"red",shape:"ring",text:"High input rate"});
-                return null;
+            // Programmatic control of resend interval using message parameter
+            if (msg.hasOwnProperty("resend_interval")) {
+                if (!isNaN(msg.resend_interval) && isFinite(msg.resend_interval)) {
+                    node.interval = msg.resend_interval * 1000; // In milliseconds
+                }
+                else {
+                    this.error("resend_interval is not a numeric value", msg);
+                }
             }
 
-            node.prevMsgTimestamp = msgTimestamp;
+            // Programmatic control of max.count using message parameter
+            if (msg.hasOwnProperty("resend_max_count")) {
+                if (!isNaN(msg.resend_max_count) && isFinite(msg.resend_max_count)) {
+                    node.maximumCount = msg.resend_max_count;
+                }
+                else {
+                    this.error("resend_max_count is not a numeric value", msg);
+                }
+            }
+
+            // Programmatic control of force cloning using message parameter
+            if (msg.hasOwnProperty("resend_force_clone")) {
+                if (msg.resend_force_clone == true || msg.resend_force_clone == false) {
+                    node.forceClone = msg.resend_force_clone;
+                }
+                else {
+                    this.error("resend_force_clone is not a boolean value", msg);
+                }
+            }
+
+            // Programmatic control to ignore this message from being cloned using message parameter
+            var ignoreMessage = false;
+            if (msg.hasOwnProperty("resend_ignore")) {
+                if (msg.resend_ignore == true || msg.resend_ignore == false) {
+                    ignoreMessage = msg.resend_ignore;
+                }
+                else {
+                    this.error("resend_ignore is not a boolean value", msg);
+                }
+            }
+            
+            if(!ignoreMessage) {
+                // As soon as a message arrives, it will be cloned to the output
+                sendMsg(msg, node);
+
+                var msgTimestamp = Date.now();
+            
+                if ((msgTimestamp - node.prevMsgTimestamp) <= node.interval) {
+                    node.status({fill:"red",shape:"ring",text:"High input rate"});
+                    return null;
+                }
+
+                node.prevMsgTimestamp = msgTimestamp;
+            }
 
             // Check whether another timer has already been started (by this node) previously
             if (node.timer !== 0) {
@@ -61,22 +104,24 @@ module.exports = function(RED) {
                node.timer = 0;
             }
 
-            // Start a new timer, that repeatedly sends the new msg to the output
-            // (with the specified milliseconds between every two repeats).
-            // The timer id will be stored, so it can be found when a new msg arrives at the input.
-            node.timer = setInterval(function() {
-                node.status({});
+            if (!ignoreMessage) {
+                // Start a new timer, that repeatedly sends the new msg to the output
+                // (with the specified milliseconds between every two repeats).
+                // The timer id will be stored, so it can be found when a new msg arrives at the input.
+                node.timer = setInterval(function() {
+                    node.status({});
 
-                if(node.maximumCount > 0 && node.msgCounter >= node.maximumCount) {
-                    // The maximum number of messages has been send, so stop the timer (for the last received input message).
-                    clearInterval(node.timer);
-                    node.timer = 0;
-                    node.status({fill:"green",shape:"dot",text:"maximum reached"});
-                }
-                else {
-                    sendMsg(msg, node);
-                }
-            }, node.interval); 
+                    if(node.maximumCount > 0 && node.msgCounter >= node.maximumCount) {
+                        // The maximum number of messages has been send, so stop the timer (for the last received input message).
+                        clearInterval(node.timer);
+                        node.timer = 0;
+                        node.status({fill:"green",shape:"dot",text:"maximum reached"});
+                    }
+                    else {
+                        sendMsg(msg, node);
+                    }
+                }, node.interval);
+            } 
         });
 
         this.on("close", function() {
